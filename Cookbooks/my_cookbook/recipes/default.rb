be3abb1996257d7898fec8aac0f84c9f5a8228ae
root <root@ip-172-31-12-124.ap-southeast-2.compute.internal>
2017-02-10 04:06:14 +0000

    add files

--- Cookbooks/my_cookbook/recipes/default.rb
+++ Cookbooks/my_cookbook/recipes/default.rb
@@ -6,14 +6,3 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-ifInstalled = `pear list Text_LanguageDetect`
-if ifInstalled.include? "not installed"
-    bash "add_textLanguageDetect" do
-      user "root"
-      cwd "/tmp"
-      code <<EOH
-      pecl channel-update pecl.php.net
-      pear install pear/Text_LanguageDetect-0.3.0
-EOH
-    end   
-end
